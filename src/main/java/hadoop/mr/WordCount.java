b15fdba17daf5ac1645e30ba05c2255bdf3f03a0
gli <ajaxchelsea@gmail.com>
2015-07-01 11:07:32 +0800

    added spark word count demo

--- src/main/java/hadoop/mr/WordCount.java
+++ src/main/java/hadoop/mr/WordCount.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package mr;
+package hadoop.mr;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
